8e32f64e0f06bcf41025f8e6141be01370aa1c30
chengzhx76 <chengzhx76@qq.com>
2019-04-16 22:22:16 +0800

    weChat event

---
+++ gotest/gotest_rest_test.go
@@ -0,0 +1,32 @@
+package gotest
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func TestRest(t *testing.T) {
+	router := mux.NewRouter()
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
+}
